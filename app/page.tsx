3cde163144c8099ed6f683367908649e6442122d
Germán Aliprandi <german@egg.live>
2024-06-08 15:50:42 -0300

    chore: Update page title in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <nav>
-        <h1>APG</h1>
+        <h1>Auto Parts Guru</h1>
         <button>Login</button>
       </nav>
     </main>
