84a039b403f7a9f2074e87dd643222a9a44836c9
Germán Aliprandi <german@egg.live>
2024-06-09 16:43:17 -0300

    chore: Refactor database connection code

--- lib/db/db.ts
+++ lib/db/db.ts
@@ -1,12 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
 export const DB = new PrismaClient();
-
-DB.$connect()
-  .then(() => {
-    console.log("🔌 Connected to database");
-  })
-  .catch((error) => {
-    console.error("❌ Error connecting to database: ", error);
-    process.exit(1);
-  });
