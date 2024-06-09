6489aacf9e0183b7b3fc8fe4d334dd73c96f3755
Germán Aliprandi <german@egg.live>
2024-06-09 10:38:58 -0300

    chore: Update NextAuth configuration with PrismaAdapter and Google profile customization

--- auth.ts
+++ auth.ts
@@ -1,6 +1,21 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { DB } from "./lib/db/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  adapter: PrismaAdapter(DB),
+  providers: [
+    Google({
+      profile(profile) {
+        return { ...profile, role: profile.role ?? "user" };
+      },
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.role = user.role;
+      return session;
+    },
+  },
 });
