a74f26d68f5fa13ecb904c44f26146cd2c8058ee
Germán Aliprandi <german@egg.live>
2024-06-17 13:30:42 -0300

    feat: Add IconButton component to UpdateUserForm for changing email

--- app/users/components/UpdateUserForm.tsx
+++ app/users/components/UpdateUserForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { userAdd } from "@/app/actions/users/user.actions";
+import { IconButton } from "@/app/components/ui/IconButton";
 import { Roles } from "@/app/services/roles.service";
 import { GetUserByEmailService } from "@/app/services/users.service";
 import Image from "next/image";
@@ -22,12 +23,7 @@ export default function UpdateUserForm({
     <form action={action}>
       <div className="grid">
         <fieldset>
-          <label>
-            Nombre
-            <input name="alias" autoComplete="off" defaultValue={alias ?? ""} />
-          </label>
-          <label>
-            Email
+          <fieldset role="group">
             <input
               type="email"
               name="email"
@@ -35,7 +31,21 @@ export default function UpdateUserForm({
               readOnly
               defaultValue={email}
             />
+            <IconButton
+              type="button"
+              className="icon error"
+              icon="edit"
+              title="Cambiar email"
+              size={30}
+              onClick={() => console.log("Change email")}
+            />
+            {/* <input type="button" value="Change" /> */}
+          </fieldset>
+          <label>
+            Nombre
+            <input name="alias" autoComplete="off" defaultValue={alias ?? ""} />
           </label>
+
           <label>
             Rol
             <select name="role" defaultValue={user?.role || "USER"}>
@@ -56,13 +66,16 @@ export default function UpdateUserForm({
             />
             Usuario activo
           </label>
+
+          <br />
+
           <input type="submit" value="Actualizar" />
           <small>{state.message}</small>
         </fieldset>
         <div className="t-center">
           <Image
             className="rounded"
-            src={user?.picture ?? "/images/avatar.png"}
+            src={user?.picture ?? "/images/avatar.webp"}
             alt={alias ?? "Avatar"}
             width={250}
             height={250}
