4483cdebb06ccb5d04ecfa747b6942899b0019b2
Germán Aliprandi <german@egg.live>
2024-06-17 14:24:31 -0300

    refactor: Update Icon component to use React hooks for managing props

--- app/components/ui/Icon.tsx
+++ app/components/ui/Icon.tsx
@@ -1,12 +1,21 @@
+"use client";
+
 import {
   FontAwesomeIcon,
   type FontAwesomeIconProps,
 } from "@fortawesome/react-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { use, useEffect, useState } from "react";
 
 library.add(fas);
 
 export default function Icon(props: FontAwesomeIconProps) {
-  return <FontAwesomeIcon {...props} />;
+  const [iconProps, setIconProps] = useState(props);
+
+  useEffect(() => {
+    setIconProps(props);
+  }, [props]);
+
+  return <FontAwesomeIcon {...iconProps} />;
 }
